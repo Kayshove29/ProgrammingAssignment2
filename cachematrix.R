8c6c578b6f5d1824a3bfaeae78c45a552a30b732
Keshav Basavapatna <kshavbas29@gmail.com>
2014-12-21 17:40:41 -0500

    Modified the original cacheMatrix file by completed
    makeCacheMatrix should now be able to create a special "Matrix" object that can cache its
    own inverse
    While cacheSolve This function computes the inverse of the special "matrix" returned by
    makeCacheMatrix while returning already calculated matrixes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## What the makeCacheMatrix function does is that it creates 
+## a special matrix object that can cache its inverse
+## The way the function works isn't all that different from the makeVector and
+## cachemean function
 makeCacheMatrix <- function(x = matrix()) {
-
+  InverseX <- NULL
+  set <- function(y) {
+    x <<- y
+    InverseX <<- NULL
+  }
+  get <- function() x
+  Set_inverse<- function(inverse) InverseX <<-inverse
+  Get_inverse <- function() InverseX
+  list(set = set, get = get,
+       Set_inverse = Set_inverse,
+       Get_inverse = Get_inverse)
 }
 
-
-## Write a short comment describing this function
+## The function cacheSolve returns the inverse of a matrix A created by
+## the makeCacheMatrix function.
+## But iff the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  InverseX <- x$getinverse()
+  if (!is.null(InverseX)) {
+    message("retrieving cached inverse matrix")
+    return(InverseX)
+  } else {
+    InverseX <- solve(x$get())
+    x$setinverse(InverseX)
+    return(InverseX)
+  }
 }
